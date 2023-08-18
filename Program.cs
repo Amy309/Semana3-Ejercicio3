a5380758415f1a7d5aaae815d45631f7731b9bed
Amy309 <125324963+Amy309@users.noreply.github.com>
2023-08-17 18:14:19 -0600

    Ejercicio3

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+Console.WriteLine("Ejercicio 3");
+
+string[] a = { "Perro", "Casa", "Cebra", "Tortuga",
+"Burro", "Mar", "Dedo", "Tasa", "Nube", "Sol"};
+
+Console.WriteLine("");
+
+Console.WriteLine("Funcion Sort");
+
+Console.WriteLine("");
+
+foreach (var i in a)
+    Console.WriteLine(i);
+
+Console.WriteLine("");
+
+System.Array.Sort(a, 0, a.Length);
+foreach (var j in a)
+    Console.WriteLine(j);
+
+Console.WriteLine("");
